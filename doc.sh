cdb3360b3bcf79b3e9b2dcd6010ec835ea8dc493
Ben Paige <benpaigeiii@hotmail.com>
2015-07-27 14:24:59 -0400

    add checks

--- doc.sh
+++ doc.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
-#get non-tag parameters
+args=("$@")
 
+NAME=$1 #Name should be the first parameter
+EXPIRE="1m" #default expire to one month
+
+#get non-tag parameters
+while true; do
      #check if expire time
+	if [[ $args(i) =~ ^([0-9][dwmy])+$ ]]
+		echo $args(i)
+	fi
 
      #check if url
+	if [[ $args(i) =~ ^https?://  ]]
+		echo $args(i)
+	fi
 
      #else get name
-
+done
 
 #get tags
